a88ed8e15d63f542feb095bc6ce6c6af3eb5c036
kauevestena <kauemv2@gmail.com>
2024-02-26 03:57:23 +0100

    to be continued...

---
+++ scripts/ABOUT.md
@@ -0,0 +1,16 @@
+# Utilities and tests using the dataset
+
+The content at analysis\raw_reports folder must be generated with [this script at the sample picker repository, since it needs CUDA-dependent libraries](https://github.com/kauevestena/deep_pavements_sample_picker/blob/main/quality_tests/test_surface_samples.py)
+
+
+# Getting started
+
+Create a virtual environment:
+
+    virtualenv dpd
+
+Install dependencies:
+
+    pip install -r scripts/requirements.txt
+
+(or simply use VSCode)
--- scripts/lib.py
+++ scripts/lib.py
@@ -3,7 +3,13 @@ import shutil
 import os
 import hashlib
 import argparse
+import pandas as pd
+import plotly.express as px
 
+raw_reports_path = os.path.join('analysis', 'raw_reports')
+
+class_label = 'class'
+prediction_label = 'prediction'
 
 def create_folder(path):
     if not os.path.exists(path):
@@ -15,3 +21,29 @@ def get_path_last_part(path):
 def get_unique_filename(foldername, filename):
     file_extension = filename.split('.')[-1]
     return hashlib.md5((foldername + filename).encode('utf-8')).hexdigest() + '.' + file_extension
+
+def read_raw_reports(inpath = raw_reports_path):
+    available_classes = os.listdir(inpath)
+
+    model_names = []
+
+    dfs_dict = {}
+    for available_class in available_classes:
+        dfs_dict[available_class] = {}
+
+        class_path = os.path.join(inpath, available_class)
+
+        available_models = os.listdir(class_path)
+
+        for available_model in available_models:
+            base_df = pd.read_csv(os.path.join(class_path, available_model))
+
+            base_df[class_label] = available_class
+
+            model_name = available_model.replace('.csv', '')
+
+            dfs_dict[available_class][model_name] = base_df
+
+            model_names.append(model_name)
+
+    return dfs_dict, [cname.replace('_', ' ') for cname in available_classes], list(set(model_names))
