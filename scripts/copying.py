953317d2c653666e6e90448d7eb7f11c0fa88058
kauevestena <kauemv2@gmail.com>
2024-04-19 17:16:24 -0300

    compacted and gravel

--- scripts/copying.py
+++ scripts/copying.py
@@ -22,20 +22,22 @@ input_folder = args.input
 
 classes = []
 
-# now iterate recursively for all folders in the input folder
-for root, dirs, filenames in os.walk(input_folder):
-    for dir in dirs:
-        create_folder(os.path.join(DATASET_PATH, dir))
-        classes.append(dir)
 
-    if root != input_folder:
-        for filename in filenames:
-            foldername = get_path_last_part(root)
+if input_folder:
+    # now iterate recursively for all folders in the input folder
+    for root, dirs, filenames in os.walk(input_folder):
+        for dir in dirs:
+            create_folder(os.path.join(DATASET_PATH, dir))
+            classes.append(dir)
 
-            # generate a new unique filename, using hashes:
-            new_filename = get_unique_filename(foldername, filename)
+        if root != input_folder:
+            for filename in filenames:
+                foldername = get_path_last_part(root)
+
+                # generate a new unique filename, using hashes:
+                new_filename = get_unique_filename(foldername, filename)
 
 
-            shutil.copy(os.path.join(root, filename), os.path.join(DATASET_PATH,foldername, new_filename))
+                shutil.copy(os.path.join(root, filename), os.path.join(DATASET_PATH,foldername, new_filename))
 
 
